6e617b5d9d9eaad7fd855a544936151fce9cf60d
Daniel Li <your_github_email@example.com>
2025-12-07 19:38:33 -0500

    added temp components to allow for routing to GroupsPage, RanksPage, ProfilePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import QuestsPage from "./components/QuestPage.jsx"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import QuestsPage from "./components/QuestPage.jsx";
 import Login from "./components/Login";
 import { Routes, Route } from "react-router-dom";
 import GetStarted from "./components/GetStarted";
 import SelectHabits from "./components/SelectHabits";
-import JoinFriends from './components/JoinFriends';
-import CreateGroup from './components/CreateGroup'
-import JoinGroup from './components/JoinGroup'
-
-
+import JoinFriends from "./components/JoinFriends";
+import CreateGroup from "./components/CreateGroup";
+import JoinGroup from "./components/JoinGroup";
+import HomePage from "./components/HomePage";
+import DuelsPage from "./components/DuelsPage";
+import ProfilePage from "./components/ProfilePage";
+import GroupsPage from "./components/GroupsPage";
+import RanksPage from "./components/RanksPage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path="/quests" element={<QuestsPage />} />
+      <Route path="/duels" element={<DuelsPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/groups" element={<GroupsPage />} />
+      <Route path="/ranks" element={<RanksPage />} />
       <Route path="/get-started" element={<GetStarted />} />
       <Route path="/select-habits" element={<SelectHabits />} />
       <Route path="/join-friends" element={<JoinFriends />} />
